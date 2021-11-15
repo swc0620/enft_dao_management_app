e95265456d7aa1a000a999e20a5db5441141d76c
deok <39942335+ziinxed@users.noreply.github.com>
2021-11-15 13:18:25 +0900

    Apply react-navigation

--- App.tsx
+++ App.tsx
@@ -2,15 +2,18 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider as PaperProvider } from "react-native-paper";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <PaperProvider>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-    </PaperProvider>
+    <NavigationContainer>
+      <PaperProvider>
+        <View style={styles.container}>
+          <Text>Open up App.tsx to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </PaperProvider>
+    </NavigationContainer>
   );
 }
 
