6d04540a3c37eb5f12d83721de0d81626b04b5aa
swc0620 <pinetreewave@naver.com>
2021-11-29 00:17:45 +0900

    modified to display portfolio detail page

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Govern from "./src/pages/Govern";
 import Portfolio from "./src/pages/Portfolio";
 import GovernDetail from "./src/pages/GovernDetail";
+import PortfolioDetail from "./src/pages/PortfolioDetail";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,38 +14,24 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    
-    
-
-    
     <PaperProvider>
-      
       <NavigationContainer>
-      
         <Stack.Navigator>
-          
-
-        <Stack.Screen name="Governance Detail" component={GovernDetail} />
-        {/* <Stack.Screen name="Governance" component={Govern} /> */}
-          
-
-          
-          
+          {/* <Stack.Screen name="Governance Detail" component={GovernDetail} /> */}
+          {/* <Stack.Screen name="Governance" component={Govern} /> */}
+          <Stack.Screen name="Portfolio Detail" component={PortfolioDetail} />
         </Stack.Navigator>
-        
-        </NavigationContainer>
-        
-      </PaperProvider>
-      
-    
-  )}
+      </NavigationContainer>
+    </PaperProvider>
+  )
+}
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
   
